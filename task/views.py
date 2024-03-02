9d3bf73c452e3f0c481a4a934aa8cbae3df87a56
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 15:48:42 -0300

    refactoring using auth.forms

--- task/views.py
+++ task/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from . import forms 
+# from . import forms 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(request):
     return render(request, "signup.html",{
-        "form": forms.SignUpForm()
+        "form": UserCreationForm()
     })
 
