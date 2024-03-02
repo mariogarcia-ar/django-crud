db8221bae64a7d73e52cba87fc497cacee24b177
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 16:54:57 -0300

    add user to tasks

--- task/models.py
+++ task/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
@@ -6,6 +8,10 @@ class Task(models.Model):
     description = models.TextField()
     # project = models.ForeignKey(Project, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now) # auto_now_add=True
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title         
