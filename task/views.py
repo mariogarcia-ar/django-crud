6a369566a8b96ccb39e02e52204771d26236c7ad
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 17:27:57 -0300

    add logout

--- task/views.py
+++ task/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
@@ -19,10 +19,9 @@ def login(request):
         # "form": UserCreationForm()
     })
 
-def logout(request):
-    return render(request, "logout.html",{
-        # "form": UserCreationForm()
-    })
+def signout(request):
+    logout(request)
+    return redirect('home')
     
 @login_required    
 def tasks(request):
