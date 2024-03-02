10f40bacdbd48561da960abfc67c6dde64db48e2
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 16:08:46 -0300

    add user

--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ python manage.py runserver
 
 python manage.py makemigrations 
 python manage.py migrate 
- 
+ python manage.py createsuperuser
 
 
 Pagina signup
--- task/views.py
+++ task/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from . import forms 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html",{
+        # "form": UserCreationForm()
+    })
+    
 def signup(request):
+    if request.method == "POST":
+        print('procesar', request.POST)
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'],
+                                     password=request.POST['password1'])   
+            user.save()
+            return redirect('home')
+
     return render(request, "signup.html",{
         "form": UserCreationForm()
     })
